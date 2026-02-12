c16f6dd8be5a1e2adb917ee7b7ebbb65a53a6017
ryasmiinn <raissayasmin05@gmail.com>
2026-02-12 22:12:32 +0700

    Enhance login script with comments and structure
    mengganti komentar sesuai dengan kebutuhan

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -5,21 +5,35 @@
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
 
+/* membuat variabel untuk setiap elemen view */
 const loginFormElement = document.querySelector('#loginForm');
 const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+/*Membuat  variabel untuk menyimpan info email dan pass */
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
+/*menambahkan aksi pada button */
 loginFormElement.addEventListener('submit', function(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  const email = inputEmailElement.value;
-  const password = inputPasswordElement.value;
-  if (email == expectedEmail && password == expectedPassword) {
-    goToHome();
-  } else {
-    showPopUp();
-  }
+    /*Mendapatkan input email dan password pengguna dari form.*/
+    const email = inputEmailElement.value;
+    const password = inputPasswordElement.value;
+   
+    /* Memastikan bahwa nilai email dan pass 
+    seusai dengan nilai yang tersimpan*/
+    if (email == expectedEmail && password == expectedPassword) {
+   
+      /* jika sesuai, maka program akan menjalankan perintah yaitu pindah ke halaman home*/
+      goToHome();
+      
+      /* jika email dan pass tidak sesuai*/
+    } else {
+      /* maka hrus tampilkan pop up bahwa input salah*/
+      showPopUp();
+    }
+
 });
+
